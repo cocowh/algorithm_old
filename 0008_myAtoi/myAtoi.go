df0dbee90c9e37b95197b2e01ccc4ceba3c53645
wuhua1 <wuhua1@100tal.com>
2019-08-08 19:49:04 +0800

    字符串转换整数

---
+++ 0008_myAtoi/myAtoi.go
@@ -0,0 +1,56 @@
+package _008_myAtoi
+
+import (
+	"math"
+	"strings"
+)
+
+
+func myAtoi(str string) int {
+
+	str = strings.Trim(str," ")
+	len := len(str)
+	if len == 0 || str == "" {
+		return 0
+	}
+	i := 0
+	result := 0
+	flag := true
+	if str[i] == '+'{
+		i++
+	} else if str[i] == '-' {
+		i++
+		flag = false
+	} else if str[i] < '0' || str[i] > '9' {
+		return 0
+	}
+	count := 0
+	for ; i < len; i++ {
+		if str[i] >= '0' && str[i] <= '9' {
+			result = result * 10 + int(str[i]) - int('0')
+			if result != 0 {
+				count++
+			}
+			if count > 10 {
+				if  flag {
+					return  math.MaxInt32
+				} else {
+					return math.MinInt32
+				}
+			}
+		} else {
+			break
+		}
+	}
+	if !flag {
+		result = -result
+	}
+	if result > math.MaxInt32 || result < math.MinInt32 {
+		if flag {
+			result = math.MaxInt32
+		} else {
+			result = math.MinInt32
+		}
+	}
+	return result
+}
