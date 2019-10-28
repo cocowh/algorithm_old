6ba4771055b0e306a363d3520aea5aa0e043aa68
cocowh <596527851@qq.com>
2019-10-28 16:43:22 +0800

    存在重复元素

---
+++ 0217_containsDuplicate/containsDuplicate.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println(containsDuplicate([]int{1, 2, 3, 4, 1}))
+}
+
+func containsDuplicate(nums []int) bool {
+	m := make(map[int]bool, len(nums))
+	for _, v := range nums {
+		if _, ok := m[v]; ok {
+			return true
+		} else {
+			m[v] = true
+		}
+	}
+	return false
+}
+
+func containsDuplicateBySort(nums []int) bool {
+	sort.Ints(nums)
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i-1] {
+			return true
+		}
+	}
+	return false
+}
